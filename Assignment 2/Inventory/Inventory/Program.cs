8bacbbc8bbf2ac6c56e33a322166884e620d972b
DixitD <hihoriyadixit1@gmail.com>
2018-02-09 16:12:51 +0530

    assignment 2

---
+++ Assignment 2/Inventory/Inventory/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inventory = new List<string>
+            { "Tomato","Potato","ToMato","Potato","Red Tomato","Tomato Catchup","Grapes","grapes","potato"};
+            string str = "tomato";
+            var count= 0;
+            Console.WriteLine("Total No of Count of tomato in list:");
+            foreach (string i in inventory)
+            {
+                if (string.Equals(i, str, StringComparison.OrdinalIgnoreCase))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+
+            Console.WriteLine("Index of Potato in List:");
+                for(int i = 0; i < inventory.Count; i++)
+                {
+                   
+                    if (inventory[i].Equals("Potato"))
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment 2/math/math/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float X = 0.06f;
+            float Y = 5.94f;
+            Console.WriteLine("X= " + X + "  Y=" + Y);
+            Console.WriteLine("1.Sum of X & Y=" + (X + Y));
+
+            if ((X + Y).Equals(6))
+            {
+                Console.WriteLine("2.Yes,X + Y = 6 is right.");
+            }
+            else
+            {
+                Console.WriteLine("2.x + y=6 is wrong");
+            }
+            int x = X.CompareTo(Y);
+            if (x == 0)
+            {
+                Console.WriteLine("3.X and Y both are equal.");
+            }
+            else if (x == -1)
+            {
+                Console.WriteLine("3.X is less than Y");
+            }
+            else
+            {
+                Console.WriteLine("3.X is Greater than Y");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
